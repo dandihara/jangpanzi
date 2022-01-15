9f3ea079bd886dca160c5e65958a3b4fb2126565
Moon <gkaqhd0000@naver.com>
2022-01-15 21:22:12 +0900

    Update models.py

--- shoes_site/models.py
+++ shoes_site/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import datetime
-from django.contrib.auth.models import User,AbstractUser,BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 
 class MyUser(AbstractUser):
